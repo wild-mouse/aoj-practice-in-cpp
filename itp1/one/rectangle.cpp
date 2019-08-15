09385812a902f05c0dc40d921bdd8fb9a12bd8e3
Tsukasa Noguchi <wildmouse0813@gmail.com>
2019-08-15 16:28:28 +0900

    Add Rectangle

---
+++ itp1/one/rectangle.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() { 
+    int a, b;
+    cin >> a >> b;
+    // cout << a << b;
+    int area = a * b;
+    int l = a * 2 + b * 2;
+    cout << area << " " << l << endl;
+    return 0;
+}
